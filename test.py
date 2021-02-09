f90c6c52e8500cdbb9c89b73c0e1863e6509f311
Rishabh Tole <rishabhtole@gmail.com>
2021-02-09 11:01:59 -0500

    testing on windows

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-import sys
-sys.path.append("/usr/local/lib/python3.8/site-packages")
-import cv2
+print("testing")
+print("testing")
+print("testing")
+print("testing")
+print("testing")
+print("testing")
